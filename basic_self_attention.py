39e753308d8fcd82fb7850d5568ebbc1843982b8
Vikram Pawar <pvikram035@gmail.com>
2024-08-05 10:26:12 -0400

    Added an example of basic self attention

---
+++ basic_self_attention.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn.functional as F
+
+# The input tensor of size b,t,k
+# b = batch size
+# t = sequence length
+# k = embedding size
+x = torch.randn([3,10,5])
+
+# Multiply the input tensor with a transposed
+# version of itself to get the unnormalized 
+# self-attention weights
+raw_weights = torch.bmm(x, x.transpose(1,2))
+
+# Do a row-wise softmax to obtain the 
+# the normalized weights
+weights = F.softmax(raw_weights, dim=2)
+
+# Multiply the weights by the input
+# to obtain the output sequence.
+# Thus, the output sequence is just a
+# weighted sum of the input sequence
+y = torch.bmm(weights, x)
+
+# print the results
+print("inputs", x)
+print("outputs", y)
